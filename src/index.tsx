c514af725669abd6eb89ef89ce2358bcd6102e71
Ezequiel <ezeq.uiel@hotmail.com>
2024-08-03 17:46:31 -0300

    add Radio Button

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,11 @@ import UFormGroupHandler from './context/form/FormGroupHandler';
 import UInput from './components/inputs/Input';
 import USelect from './components/inputs/Select';
 import UFormButton from './components/buttons/FormButton';
+import URadioButton from './components/inputs/RadioButton';
 
 export const FormHandler = UFormHandler;
 export const FormGroupHandler = UFormGroupHandler;
 export const Input = UInput;
 export const Select = USelect;
 export const FormButton = UFormButton;
+export const RadioButton = URadioButton;
