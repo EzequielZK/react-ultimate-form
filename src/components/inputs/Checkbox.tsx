1e7f1cd5b653fef0eef939bd485e2cf3f32e1318
Ezequiel <ezeq.uiel@hotmail.com>
2024-08-08 15:38:00 -0300

    add Checkbox component

---
+++ src/components/inputs/Checkbox.tsx
@@ -0,0 +1,76 @@
+import * as React from 'react'
+
+import MuiCheckbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import useFormGroupHandler from "@/hooks/useFormGroupHandler";
+import type { CustomCheckboxProps } from "./types";
+import { useTransition } from "react";
+
+export default function Checkbox({
+  name,
+  label,
+  required,
+  variant,
+  fontWeight,
+  defaultValue = false,
+  autoSubmit,
+  onChange,
+  ...props
+}: CustomCheckboxProps) {
+  const { setValue, setError, submit, data } = useFormGroupHandler({
+    name,
+    required,
+    label,
+    defaultValue,
+  });
+  const { value, errorMessage } = data;
+  const [, setTransition] = useTransition();
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    checked: boolean
+  ) => {
+    setValue(checked);
+
+    if (errorMessage) {
+      setError(null);
+    }
+    if (autoSubmit) {
+      submit({ [name]: checked });
+    }
+    if (onChange) {
+      setTransition(() => onChange(event, checked));
+    }
+  };
+  return (
+    <Stack direction="column">
+      <FormControlLabel
+        {...props}
+        control={
+          <MuiCheckbox
+            checked={value}
+            onChange={handleChange}
+            sx={{
+              fontSize: variant ?? "body1.fontSize",
+              fontWeight: fontWeight,
+              color: Boolean(errorMessage) ? "error.main" : "action.disabled",
+            }}
+          />
+        }
+        label={label}
+      />
+      {errorMessage && (
+        <Typography
+          component="span"
+          color="error"
+          variant="caption"
+          sx={{ m: 0 }}
+        >
+          {errorMessage}
+        </Typography>
+      )}
+    </Stack>
+  );
+}
